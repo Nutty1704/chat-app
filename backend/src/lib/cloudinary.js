741d64c5e05f8098a4c3ea6c5adfc16212ba86f8
Abhijit Upadhyay <abhijitupadhyay03@gmail.com>
2024-11-22 18:18:27 +1100

    Add backend

---
+++ backend/src/lib/cloudinary.js
@@ -0,0 +1,10 @@
+import {v2 as cloduinary} from 'cloudinary';
+
+cloduinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+export default cloduinary;
+
